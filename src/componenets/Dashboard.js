4709c8d9ef76ca76ebd1c8117a7504f1b627c374
sandesnp <averagenp@gmail.com>
2020-02-09 17:52:44 +0545

    added slider and cardbody

--- src/componenets/Dashboard.js
+++ src/componenets/Dashboard.js
@@ -1,7 +1,20 @@
-import React, { Component, useState } from 'react';
+import React, { Component, useState, Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import RBCarousel from 'react-bootstrap-carousel';
-import { Row, Col, Button, ButtonGroup } from 'reactstrap';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import {
+	Row,
+	Col,
+	Button,
+	ButtonGroup,
+	Card,
+	CardImg,
+	CardText,
+	CardBody,
+	CardTitle,
+	CardSubtitle
+} from 'reactstrap';
 import 'react-bootstrap-carousel/dist/react-bootstrap-carousel.css';
 import Background01 from '../images/background.jpg';
 import Background02 from '../images/login_background.jpg';
@@ -9,7 +22,28 @@ import Background03 from '../images/register_background.jpg';
 import axios from 'axios';
 
 const styles = { height: 400, width: '100%' };
-let token, profileretrieved;
+let token,
+	profileretrieved,
+	advertisementsRetrieved,
+	ThreeCard = [];
+
+const responsive = {
+	desktop: {
+		breakpoint: { max: 3000, min: 1024 },
+		items: 2,
+		slidesToSlide: 1 // optional, default to 1.
+	},
+	tablet: {
+		breakpoint: { max: 1024, min: 464 },
+		items: 2,
+		slidesToSlide: 2 // optional, default to 1.
+	},
+	mobile: {
+		breakpoint: { max: 464, min: 0 },
+		items: 1,
+		slidesToSlide: 1 // optional, default to 1.
+	}
+};
 
 export default class Dashboard extends Component {
 	constructor(props) {
@@ -17,6 +51,23 @@ export default class Dashboard extends Component {
 		this.state = {
 			usersretrieved: profileretrieved
 		};
+
+		axios.get('http://localhost:3001/adver/all').then(response => {
+			localStorage.setItem('advertisements', JSON.stringify(response.data));
+		});
+		//retrieveing advertise images
+		advertisementsRetrieved = JSON.parse(
+			localStorage.getItem('advertisements')
+		);
+
+		//if advertisementsRetrieved has any objects, for each on its objects and see if it has title 'dashboardcard' and if it does then save on different array.
+		if (advertisementsRetrieved) {
+			advertisementsRetrieved.forEach(element => {
+				if (element.position === 'dashboardcard') {
+					ThreeCard.push(element);
+				}
+			});
+		}
 
 		if (localStorage.getItem('token')) {
 			token = {
@@ -34,6 +85,7 @@ export default class Dashboard extends Component {
 	render() {
 		return (
 			<div>
+				{/*----------------------Section 1 */}
 				<div
 					className="container-fluid"
 					style={{
@@ -84,12 +136,63 @@ export default class Dashboard extends Component {
 						</Col>
 					</Row>
 				</div>
+				{/*----------------------Section 2 */}
+				<Row className="col-12" style={{ marginLeft: 5 }}>
+					{ThreeCard.map((card, key) => (
+						<Card className="col-4" key={card.id}>
+							{' '}
+							{/*No key for now*/}
+							<CardImg
+								top
+								width="100%"
+								src={`http://localhost:3001/uploads/${card.image}`}
+								alt="Card image cap"
+							/>
+							<CardBody>
+								<CardTitle>{card.title}</CardTitle>
+								<CardSubtitle>Card subtitle</CardSubtitle>
+								<CardText>{card.description}</CardText>
+								<Button>Button</Button>
+							</CardBody>
+						</Card>
+					))}
+				</Row>
+				;{/*----------------------Section 3 */}
 				<div>
-					{localStorage.getItem('token') ? (
-						<h3 style={{ color: 'white' }}>{profileretrieved.firstname}</h3>
-					) : (
-						<h3>No data retrieved</h3>
-					)}
+					<Carousel
+						additionalTransfrom={0}
+						arrows
+						autoPlaySpeed={3000}
+						centerMode={false}
+						containerClass="container-with-dots"
+						draggable
+						focusOnSelect={false}
+						infinite
+						keyBoardControl
+						minimumTouchDrag={80}
+						renderButtonGroupOutside={false}
+						renderDotsOutside={false}
+						responsive={responsive}
+						showDots={false}
+						slidesToSlide={1}
+						swipeable
+					>
+						{ThreeCard.map((card, key) => (
+							<Card className="col-10" key={card.id}>
+								{' '}
+								{/*No key for now*/}
+								<CardImg
+									top
+									width="100%"
+									src={`http://localhost:3001/uploads/${card.image}`}
+									alt="Card image cap"
+								/>
+								<CardBody>
+									<CardSubtitle>Card subtitle</CardSubtitle>
+								</CardBody>
+							</Card>
+						))}
+					</Carousel>
 				</div>
 			</div>
 		);
